08cdff8b850c529cf3f112ed554ee2b9d2f11c63
Morita Taro <38848223+taro0079@users.noreply.github.com>
2024-05-07 01:02:47 +0900

    add session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destory'
   resources :articles, only: %i[index show create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe "Articles", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/articles/index"
+      get "/articles"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/articles/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "POST /create" do
+  #   it "returns http success" do
+  #     post "/articles/create"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
