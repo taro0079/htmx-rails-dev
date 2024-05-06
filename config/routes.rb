cf8f178cf6b77a72f79a99dec6ed63e6c995ffdc
Morita Taro <38848223+taro0079@users.noreply.github.com>
2024-05-06 15:54:27 +0900

    show for article

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/create'
+  resources :articles, only: %i[index show create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
